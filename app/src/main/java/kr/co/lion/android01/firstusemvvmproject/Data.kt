dd2624b6c162989c513a754b1b3bff8a0f4224da
seongukHeo <heoseonguk3@gmail.com>
2024-03-17 00:00:50 +0900

    feat : 회원가입 유효성 검사 추가

--- app/src/main/java/kr/co/lion/android01/firstusemvvmproject/Data.kt
+++ app/src/main/java/kr/co/lion/android01/firstusemvvmproject/Data.kt
@@ -1,6 +1,16 @@
 package kr.co.lion.android01.firstusemvvmproject
 
+import android.content.Context
+import android.content.DialogInterface
+import android.os.SystemClock
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlin.concurrent.thread
+
 class Data {
+
 }
 
 enum class FragmentName(var str: String){
@@ -8,4 +18,32 @@ enum class FragmentName(var str: String){
     JOIN_FRAGMENT("JoinFragment"),
     SEARCH_ID_FRAGMENT("SearchIdFragment"),
     SEARCH_PW_FRAGMENT("SearchPwFragment")
+}
+//Dialog를 보여주는 확장함수
+fun Context.showDialog(title:String, message:String, listener: (DialogInterface, Int) -> Unit){
+    var dialog = MaterialAlertDialogBuilder(this)
+    dialog.setTitle(title)
+    dialog.setMessage(message)
+    dialog.setPositiveButton("확인", listener)
+    dialog.setNegativeButton("취소", null)
+    dialog.show()
+}
+
+//키보드를 올려주는 확장함수
+fun Context.showSoftInput(view:View, context: Context){
+    view.requestFocus()
+
+    thread {
+        SystemClock.sleep(1000)
+        val inputMethodManager = context.getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(view, 0)
+    }
+}
+
+//키보드를 내려주는 확장함수
+fun Context.hideSoftInput(activity: AppCompatActivity){
+    if (activity.window.currentFocus != null){
+        val inputMethodManager = getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(activity.window.currentFocus?.windowToken, 0)
+    }
 }
--- app/src/main/java/kr/co/lion/android01/firstusemvvmproject/fragment/JoinFragment.kt
+++ app/src/main/java/kr/co/lion/android01/firstusemvvmproject/fragment/JoinFragment.kt
@@ -1,41 +1,52 @@
 package kr.co.lion.android01.firstusemvvmproject.fragment
 
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import kr.co.lion.android01.firstusemvvmproject.FragmentName
 import kr.co.lion.android01.firstusemvvmproject.MainActivity
 import kr.co.lion.android01.firstusemvvmproject.R
 import kr.co.lion.android01.firstusemvvmproject.databinding.FragmentJoinBinding
+import kr.co.lion.android01.firstusemvvmproject.hideSoftInput
+import kr.co.lion.android01.firstusemvvmproject.showDialog
+import kr.co.lion.android01.firstusemvvmproject.showSoftInput
 import kr.co.lion.android01.firstusemvvmproject.viewModel.JoinViewModel
+import java.util.regex.Pattern
 
 class JoinFragment : Fragment() {
 
-    lateinit var fragmentJoinBinding:FragmentJoinBinding
+    lateinit var fragmentJoinBinding: FragmentJoinBinding
     lateinit var mainActivity: MainActivity
 
-    lateinit var joinViewModel:JoinViewModel
+    lateinit var joinViewModel: JoinViewModel
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         //fragmentJoinBinding = FragmentJoinBinding.inflate(layoutInflater)
-        fragmentJoinBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_join, container, false)
+        fragmentJoinBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_join, container, false)
         joinViewModel = JoinViewModel()
         fragmentJoinBinding.joinViewModel = joinViewModel
         fragmentJoinBinding.lifecycleOwner = this
 
         mainActivity = activity as MainActivity
         settingToolBar()
+        setEvent()
+        setView()
         return fragmentJoinBinding.root
     }
 
     //툴바 설정
-    private fun settingToolBar(){
+    private fun settingToolBar() {
         fragmentJoinBinding.apply {
             materialToolbar2.apply {
                 title = "회원가입"
@@ -47,7 +58,135 @@ class JoinFragment : Fragment() {
         }
     }
 
-    //
+    //이벤트 설정
+    private fun setEvent() {
+        fragmentJoinBinding.apply {
+            buttonJoin.setOnClickListener {
+                val chk = checkEmptyText()
+                if (chk == true) {
+                    checkOK()
+                    mainActivity.hideSoftInput(mainActivity)
+                }
+            }
+        }
+    }
+
+    //화면 설정
+    private fun setView() {
+        fragmentJoinBinding.apply {
+
+            //입력 요소 초기화
+            joinViewModel!!.textJoinName.value = ""
+            joinViewModel!!.textJoinNumber.value = ""
+            joinViewModel!!.textJoinId.value = ""
+            joinViewModel!!.textJoinPw.value = ""
+            joinViewModel!!.textJoinCheckPw.value = ""
+
+
+            //포커스 주기
+            mainActivity.showSoftInput(textJoinName, mainActivity)
+
+            //에러 해결
+            textJoinName.addTextChangedListener {
+                textJoinNameLayout.error = null
+            }
+            textJoinNumber.addTextChangedListener {
+                textJoinNumberLayout.error = null
+            }
+            textJoinId.addTextChangedListener {
+                textJoinIdLayout.error = null
+            }
+            textJoinPw.addTextChangedListener {
+                textJoinPwLayout.error = null
+            }
+        }
+    }
+
+    //빈 TextField가 있는지 확인한다
+    private fun checkEmptyText(): Boolean {
+        fragmentJoinBinding.apply {
+            var errorText: View? = null
+
+            val name = joinViewModel!!.textJoinName.value!!
+            val number = joinViewModel!!.textJoinNumber.value!!
+            val userId = joinViewModel!!.textJoinId.value!!
+            val userPw = joinViewModel!!.textJoinPw.value!!
+
+            //이름
+            if (name.trim().isEmpty()) {
+                textJoinNameLayout.error = "이름을 입력해주세요"
+                if (errorText == null) {
+                    errorText = textJoinName
+                } else {
+                    textJoinNameLayout.error = null
+                }
+            }
+
+            //핸드폰 번호
+            if (number.trim().isEmpty()) {
+                textJoinNumberLayout.error = "휴대폰 번호를 입력해주세요"
+                if (errorText == null) {
+                    errorText = textJoinNumber
+                } else {
+                    textJoinNumberLayout.error = null
+                }
+            }
+            //아이디
+            if (userId.trim().isEmpty()) {
+                textJoinIdLayout.error = "아이디를 입력해주세요"
+                if (errorText == null) {
+                    errorText = textJoinId
+                } else {
+                    textJoinIdLayout.error = null
+                }
+            }
+            //비밀번호
+            if (userPw.trim().isEmpty()) {
+                textJoinPwLayout.error = "비밀번호를 입력해주세요"
+                if (errorText == null) {
+                    errorText = textJoinPw
+                } else {
+                    textJoinPwLayout.error = null
+                }
+            }
+            if (errorText != null) {
+                mainActivity.showSoftInput(errorText, mainActivity)
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+
+    //중복 오류 체크
+    private fun checkOK() {
+        fragmentJoinBinding.apply {
+            //val userId = joinViewModel!!.textJoinId.value!!
+            val userPw = joinViewModel!!.textJoinPw.value!!
+            val checkPw = joinViewModel!!.textJoinCheckPw.value!!
+
+            //아이디는 중복 파이어 베이스를 이용한 중복 체크이므로 나중에!
+
+            //비밀번호 특수문자 입력 받기
+            val specialText = Pattern.compile("[!@#$%^&*)(+=.,;:]")
+            val matchers = specialText.matcher(userPw)
+
+            if (!matchers.find()) {
+                textView.isVisible = true
+                return
+            } else if (userPw != checkPw) {
+                mainActivity.showDialog(
+                    "비밀번호 입력 오류",
+                    "비밀번호가 틀립니다"
+                ) { dialogInterface: DialogInterface, i: Int ->
+                    mainActivity.showSoftInput(textJoinPw, mainActivity)
+                }
+                return
+            }
+            mainActivity.removeFragment(FragmentName.JOIN_FRAGMENT)
+
+        }
+    }
 }
 
 
